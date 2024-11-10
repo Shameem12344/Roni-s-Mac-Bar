bdcbff81feeeaa68cf0f8b984db9205ce1bdef1a
Shameem12344 <153969524+Shameem12344@users.noreply.github.com>
2024-11-10 10:53:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ with st.expander("Detailed Statistics"):
 
 # Extra section on Shirt Data:
 # Load shirts data
-elif view_mode == "Shirt Analysis":
+else view_mode == "Shirt Analysis":
     # Load shirts data
     @st.cache_data
     def load_shirt_data():
